085b3bf661ddff133692ef723ef3fe71e44e9024
강지원 <jiwon@example.com>
2025-12-31 18:45:45 +0900

    시작이다...

--- src/main/java/org/example/Head02_JavaProgramming/WhileDoWhileExample.java
+++ src/main/java/org/example/Head02_JavaProgramming/WhileDoWhileExample.java
@@ -1,5 +1,12 @@
 package org.example.Head02_JavaProgramming;
 
 public class WhileDoWhileExample {
-
+    public static void main(String[] args) {
+//        int count = 0;
+//
+//        while (count < 3){
+//            System.out.println("count = " + count);
+//            count++;
+//        }
+    }
 }
