4bd1961f5d1f79fa94912493f037453122988bc9
강지원 <jiwon@example.com>
2026-01-03 01:18:53 +0900

    Ignore secret.txt

--- src/main/java/org/example/Head02_JavaProgramming/DefaultValueCheck.java
+++ src/main/java/org/example/Head02_JavaProgramming/DefaultValueCheck.java
@@ -8,5 +8,7 @@ public class DefaultValueCheck {
         System.out.println("flags[0] : " + flags[0]);
         System.out.println("numbers[0] : " + numbers[0]);
         System.out.println("strings[0] : " + strings[0]);
+
+
     }
 }
