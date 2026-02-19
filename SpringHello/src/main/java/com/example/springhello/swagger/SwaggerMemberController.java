6c1b11a94f36b1523bffefa316079ee814a31973
apple <kjw010900@gmail.com>
2026-02-19 18:33:55 +0900

    feat: practice SwaggerApplication

---
+++ SpringHello/src/main/java/com/example/springhello/swagger/SwaggerMemberController.java
@@ -0,0 +1,24 @@
+package com.example.springhello.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import com.example.springhello.swagger.SwaggerMemberResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Member API")
+@RestController
+@RequestMapping("/api/members")
+public class SwaggerMemberController {
+
+  @Operation(summary = "회원 조회")
+  @GetMapping("/{id}")
+  public SwaggerMemberResponse getMember(
+      @Parameter(description = "회원 ID", example = "1")
+      @PathVariable Long id) {
+    return new SwaggerMemberResponse(id, "kim", "kim@test.com");
+  }
+}
